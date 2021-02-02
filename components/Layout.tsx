5d1e7bce696745e069e7eff5f53e492139ddca31
empitsu <yoshie8080@gmail.com>
2021-02-03 03:45:26 +0900

    feat: update Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
+
 type Props = {
   children: React.ReactNode;
 };
 
 function Layout({ children }: Props) {
-  return <div>{children}</div>;
+  return (
+    <>
+      <nav>
+        <Link href="/mypage/top">もどる</Link>
+      </nav>
+      <div>{children}</div>
+    </>
+  );
 }
 
 export default Layout;
