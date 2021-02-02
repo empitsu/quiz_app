99f4c2e6393340c9c5f441ea71293ed0938cf65e
empitsu <yoshie8080@gmail.com>
2021-02-02 13:13:16 +0900

    chore: add eslint-plugin-react-hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,19 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react-hooks"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
   ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn", // Checks effect dependencies
+  },
 };
