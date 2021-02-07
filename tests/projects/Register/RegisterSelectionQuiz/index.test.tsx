65d89d9eff1ea6c5271046611055a7e9403f69df
empitsu <yoshie8080@gmail.com>
2021-02-08 02:49:18 +0900

    test: modify RegisterSelectionQuiz

--- tests/projects/Register/RegisterSelectionQuiz/index.test.tsx
+++ tests/projects/Register/RegisterSelectionQuiz/index.test.tsx
@@ -1,5 +1,7 @@
 import { render, fireEvent } from "@testing-library/react";
-import RegisterSelectionQuiz from "../../../../src/projects/Register/RegisterSelectionQuiz";
+import RegisterSelectionQuiz, {
+  SelectionQuiz,
+} from "../../../../src/projects/Register/RegisterSelectionQuiz";
 import { delayEventLoop } from "../../../delayEventLoop";
 import { postQuiz } from "../../../../src/utils/postQuiz";
 
@@ -8,7 +10,7 @@ jest.spyOn(window, "alert").mockImplementation(jest.fn());
 jest.mock("../../../../src/utils/postQuiz", () => {
   return {
     postQuiz: jest.fn().mockImplementation(() => {
-      Promise.resolve();
+      return Promise.resolve;
     }),
   };
 });
@@ -37,7 +39,7 @@ function submitWithValidValues({
 describe("RegisterSelectionQuiz", () => {
   describe("An error message for title of the quiz", () => {
     it("should be displayed when the value is empty", async () => {
-      const { getByLabelText, queryByRole, getByText, debug } = render(
+      const { getByLabelText, queryByRole, getByText } = render(
         <RegisterSelectionQuiz />
       );
       const titleInputElement = getByLabelText("問題文");
@@ -63,6 +65,22 @@ describe("RegisterSelectionQuiz", () => {
       ).toBeInTheDocument();
     });
   });
+  // SelectionQuizOption
+  describe("An error message for an option", () => {
+    it("should be displayed when the value is empty", async () => {
+      const { getByLabelText, queryByRole, getByText } = render(
+        <RegisterSelectionQuiz />
+      );
+      const optionInputElement = getByLabelText("選択肢1");
+      fireEvent.input(optionInputElement, { target: { value: "" } });
+      const submitBtn = getByText("クイズを登録する");
+      fireEvent.submit(submitBtn);
+      await delayEventLoop();
+      expect(
+        queryByRole("alert", { name: "選択肢1のエラー" })
+      ).toBeInTheDocument();
+    });
+  });
   describe("postQuiz", () => {
     it("should be called with quiz when all values are valid and then submit button is clicked", async () => {
       const { getByLabelText, getByText } = render(<RegisterSelectionQuiz />);
@@ -78,7 +96,19 @@ describe("RegisterSelectionQuiz", () => {
       });
 
       await delayEventLoop();
-      expect(postQuiz).toHaveBeenCalledTimes(1);
+      const expectedData: SelectionQuiz = {
+        type: "selection",
+        title: "test",
+        correctOptionId: 1,
+        options: [
+          { optionId: 1, text: "test" },
+          { optionId: 2, text: "test" },
+          { optionId: 3, text: "test" },
+          { optionId: 4, text: "test" },
+        ],
+      };
+
+      expect(postQuiz).toHaveBeenCalledWith(expectedData);
       expect.assertions(1);
     });
   });
