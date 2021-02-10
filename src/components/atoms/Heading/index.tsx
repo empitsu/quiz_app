84ecd0d38cff6b6898eda388a3266c0b3c876ed6
empitsu <yoshie8080@gmail.com>
2021-02-11 01:41:38 +0900

    feat: add h3-h5 style to Heading

--- src/components/atoms/Heading/index.tsx
+++ src/components/atoms/Heading/index.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import styled, { StyledComponentInnerOtherProps } from "styled-components";
 
-const StyledPageTitleH1 = styled.h1<{
+type StyledProps = {
   styleLevel: "h1" | "h2" | "h3" | "h4";
-}>`
+};
+
+const StyledHeadingH2 = styled.h1<StyledProps>`
   position: relative;
   text-align: center;
   ${({ theme, styleLevel }) => theme.typography[styleLevel]}
-  padding-bottom: 0.7rem;
+  padding-bottom: 1.0rem;
 
   ::before {
     position: absolute;
-    bottom: -10px;
+    bottom: 0;
     left: calc(50% - 30px);
     width: 60px;
     height: 5px;
@@ -21,15 +23,33 @@ const StyledPageTitleH1 = styled.h1<{
   }
 `;
 
-type Props = StyledComponentInnerOtherProps<typeof StyledPageTitleH1> &
+const StyledHeading = styled.h1<StyledProps>`
+  position: relative;
+  text-align: center;
+  ${({ theme }) => theme.typography.h3}
+`;
+
+type Props = StyledComponentInnerOtherProps<typeof StyledHeading> &
   React.HTMLAttributes<HTMLHeadingElement>;
 
 export const Heading = React.forwardRef<HTMLDivElement, Props>(
-  ({ children, ...otherProps }, ref) => (
-    <StyledPageTitleH1 ref={ref} {...otherProps}>
-      <span>{children}</span>
-    </StyledPageTitleH1>
-  )
+  ({ styleLevel, children, ...otherProps }, ref) => {
+    switch (styleLevel) {
+      case "h2":
+        return (
+          <StyledHeadingH2 styleLevel={styleLevel} ref={ref} {...otherProps}>
+            <span>{children}</span>
+          </StyledHeadingH2>
+        );
+        break;
+      default:
+        return (
+          <StyledHeading styleLevel={styleLevel} ref={ref} {...otherProps}>
+            {children}
+          </StyledHeading>
+        );
+    }
+  }
 );
 
 Heading.displayName = "Heading";
