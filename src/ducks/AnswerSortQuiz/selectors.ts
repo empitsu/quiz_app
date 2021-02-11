c2bc7f7fe13a626f8dd3d83236387ff17bdab9b9
empitsu <yoshie8080@gmail.com>
2021-02-12 03:20:39 +0900

    fix: fix bug in checking the correct answer

--- src/ducks/AnswerSortQuiz/selectors.ts
+++ src/ducks/AnswerSortQuiz/selectors.ts
@@ -2,6 +2,7 @@ import { SortableOptionsSet } from "./model";
 
 export function isAnswerCorrect(state: SortableOptionsSet): boolean {
   if (state.selectedOptions.length === 0) return false;
+  if (state.selectedOptions.length !== state.restOptions.length) return false;
   return state.selectedOptions.every((option, index) => {
     return option.optionId === index + 1;
   });
